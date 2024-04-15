0c46b1d6cced1b7b8c5ef639845dc82f20b11577
echapman26 <echapman26@my.whitworth.edu>
2024-04-14 17:25:04 -0700

    worked on profile formatting, added edit button skeleton

--- app2/studentcompanion_firstprototype/lib/profile.dart
+++ app2/studentcompanion_firstprototype/lib/profile.dart
@@ -27,27 +27,34 @@ class ProfileShell extends StatelessWidget{
       appBar: AppBar(
         title: const Text("Whitworth University"),
         actions: [
-            ProfilePhoto(
-              totalWidth: 31, // sets diameter
-              cornerRadius: 21, // sets corner curvature
-              color: Colors.black, // color to show if no image
-              image: user_image, // sets image to display
-              // TODO(any): add profile editing functionality by tapping photo
-              //onTap: // sets what to do on tap
+            IconButton(
+              onPressed: () => print("WIP"), // WIP for editing icon
+              icon: const Icon(Icons.edit),
             ),
-            const Padding(padding: EdgeInsets.all(10)), // adds padding to the right of the icon
         ],
       ),
       // TODO(any): integrate storage & modification of user data
-      body: const Column(
-        children: [
-          // TODO(any): replace text widgets with more complex display widgets
-          Text("atest"), // User's username (assumedly of form atest11)
-          Text("atest11@my.whitworth.edu"), // User's email
-          Text("555-555-5555"), // User's phone number
-          Text("Whitworth CS, Example, Etc"), // list of user's interests
-        ],
-      )
+      body: Center(
+        child: Column(
+          children: [
+            const Padding(padding: EdgeInsets.all(10)), // adds padding to the top of the icon
+            ProfilePhoto(
+                totalWidth: 300, // sets diameter
+                cornerRadius: 300, // sets corner curvature
+                color: Colors.black, // color to show if no image
+                image: user_image, // sets image to display
+                // TODO(any): add profile editing functionality by tapping photo
+                //onTap: // sets what to do on tap
+              ),
+            // TODO(any): replace text widgets with more complex display widgets
+            const Padding(padding: EdgeInsets.all(10)), // adds padding to the bottom of the icon
+            Text("atest"), // User's username
+            Text("atest11@my.whitworth.edu"), // User's email
+            Text("555-555-5555"), // User's phone number
+            Text("Whitworth CS, Example, Etc"), // list of user's interests
+          ],
+        ),
+      ),
     );
   }
 }
