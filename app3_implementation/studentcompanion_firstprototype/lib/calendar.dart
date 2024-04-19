3a60e9c7df7a256332295cdd9e9379b763402a18
bbahrami23 <bbahrami23@my.whitworth.edu>
2024-04-19 14:36:29 -0700

    calendar changes to stateful widgets

--- app3_implementation/studentcompanion_firstprototype/lib/calendar.dart
+++ app3_implementation/studentcompanion_firstprototype/lib/calendar.dart
@@ -3,10 +3,11 @@
 ////////
 
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
+// ignore: depend_on_referenced_packages
+import 'package:flutter_event_calendar/flutter_event_calendar.dart';
 
 class Calendar extends StatefulWidget {
-  const Calendar({Key? key}) : super(key: key);
+  const Calendar({super.key});
 
   @override
   _CalendarState createState() => _CalendarState();
@@ -14,6 +15,7 @@ class Calendar extends StatefulWidget {
 
 class _CalendarState extends State<Calendar> {
   late DateTime _today; // Variable to store the current date
+  // ignore: unused_field
   late DateTime _aWeekFromNow; // Variable to store the date a week from now
 
   @override
@@ -27,25 +29,20 @@ class _CalendarState extends State<Calendar> {
 
   @override
   Widget build(BuildContext context) {
-    return TableCalendar(
-      // Set the first selectable day for the calendar
-      firstDay: DateTime.utc(2020, 1, 1),
-      // Set the last selectable day for the calendar
-      lastDay: DateTime.utc(2030, 12, 31),
-      // Set the initially focused day for the calendar
-      focusedDay: _today,
-      // Function to load events for each day in the calendar
-      eventLoader: (day) {
-        // Check if the day is a week from now
-        if (day.day == _aWeekFromNow.day &&
-            day.month == _aWeekFromNow.month &&
-            day.year == _aWeekFromNow.year) {
-          // Return a list with an event for that day
-          return ['Event on this day'];
-        }
-        // Return an empty list if no event for this day
-        return [];
-      },
+    return EventCalendar(
+      calendarType: CalendarType.GREGORIAN,
+      calendarLanguage: 'en',
+      events: [
+        Event(
+          child: const Text('Laravel Event'),
+          dateTime: CalendarDateTime(
+            year: 2024,
+            month: 04,
+            day: 19,
+            calendarType: CalendarType.JALALI,
+          ),
+        ),
+      ],
     );
   }
 }
